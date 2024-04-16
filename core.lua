a346912dff36d4905966764eea6bb43ccefb34b6
Ranoth <47944849+Ranoth@users.noreply.github.com>
2024-04-16 19:10:23 +0200

    Fix: Move events registering to OnEnable()

--- core.lua
+++ core.lua
@@ -2,28 +2,21 @@ local SpellMessages = RanothUtils:GetModule("SpellMessages")
 local Commands = RanothUtils:GetModule("Commands")
 
 function RanothUtils:UNIT_SPELLCAST_SENT(self, unit, _, _, spellId)
-    RanothUtils:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-    RanothUtils:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
     SpellMessages:PlayerCastSent(unit, _, _, spellId)
 end
 
 function RanothUtils:UNIT_SPELLCAST_START(self, unit, castGUID, spellId)
-    RanothUtils:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-    RanothUtils:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+
     SpellMessages:NpcCastStart(unit, castGUID, spellId)
 end
 
 function RanothUtils:UNIT_SPELLCAST_INTERRUPTED(self, unit, _, spellId)
     if unit == "pet" then return end
-    RanothUtils:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-    RanothUtils:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
     SpellMessages:PlayerCastInterrupted(unit, _, spellId)
 end
 
 function RanothUtils:UNIT_SPELLCAST_SUCCEEDED(self, unit, _, spellId)
     if unit == "pet" then return end
-    RanothUtils:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-    RanothUtils:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
     SpellMessages:PlayerCastSucceeded(unit, _, spellId)
     SpellMessages:NpcCastSucceeded(unit, _, spellId)
 end
@@ -44,6 +37,8 @@ end
 function RanothUtils:OnEnable()
     RanothUtils:RegisterEvent("UNIT_SPELLCAST_SENT")
     RanothUtils:RegisterEvent("UNIT_SPELLCAST_START")
+    RanothUtils:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+    RanothUtils:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
     RanothUtils:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 end
 
