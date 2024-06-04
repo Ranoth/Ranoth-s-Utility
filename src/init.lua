ee30fcb70c9910c0e8d28dcad32d0b42dc442983
Ranoth <47944849+Ranoth@users.noreply.github.com>
2024-06-04 22:06:33 +0200

    ci: testing auto changelog for cf

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@
 --- @module "Init"
 
 local addon_name, _ = ...
-local RanothUtils = LibStub("AceAddon-3.0"):NewAddon(addon_name, "AceEvent-3.0", "AceHook-3.0")
+local RanothUtils = LibStub("AceAddon-3.0"):NewAddon(addon_name, "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
 
 local defaults = {
     profile = {
